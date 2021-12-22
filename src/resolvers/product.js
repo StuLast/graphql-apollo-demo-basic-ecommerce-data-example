f98db325ec2ef221f0845c449737c378ddf91d8a
Stu Last <stuart@spycedconcepts.uk>
2021-12-22 02:55:06 +0000

    Implements reviews for products, code refactor

---
+++ src/resolvers/product.js
@@ -0,0 +1,10 @@
+const Product = {
+  category({ categoryId }, args, { categories }, info) {
+    return categories.find((category) => category.id === categoryId);
+  },
+  reviews({ id }, args, { reviews }, info) {
+    return reviews.filter((review) => review.productId === id);
+  }
+}
+
+export { Product };
